be8e50161101dc169f005d55accafaa311943dbf
Adam Royle <adam@bigfish.tv>
2017-07-06 08:20:49 +1000

    Support multiple package.json and prettier configs and updated config caching.

--- auto-prettier.py
+++ auto-prettier.py
@@ -5,54 +5,97 @@ import re
 import subprocess
 import shlex
 import fnmatch
-#import time
+import time
+import json
 
 class AutoPrettier(sublime_plugin.EventListener):
 	
 	node_path = '/usr/local/bin/node'
-	root_path = 'asdfasdfasd'
-	prettier_glob = ''
-	prettier_path = ''
-	prettier_options = []
+	packages = {}
 
 	def on_post_save_async(self, view):
-		#self.start = time.perf_counter()
-
-		if not view.file_name().startswith(self.root_path):
-			
-			package = self.get_package_json(view)
-			if package is False:
-				return
-			root = os.path.dirname(package)
-
-			self.root_path = root
-			
-			self.prettier_path = root + '/node_modules/.bin/prettier'
-
-			file = open(package, 'r')
-			json = file.read()
-			m = re.search("prettier (.+?) --write '(.+)'", json)
-			file.close()
-			options = m.group(1)
-			glob = m.group(2)
-
-			self.prettier_options = shlex.split(options)
-			self.prettier_glob = root + '/' + glob.replace('**/*', '*')
+		self.start = time.perf_counter()
 		
-		if fnmatch.fnmatch(view.file_name(), self.prettier_glob):
-			self.run_prettier(view)		
+		packages = self.get_packages(view)
 
-	def run_prettier(self, view):
-		cmd = [self.node_path, self.prettier_path] + self.prettier_options + ['--write', view.file_name()]
-		#print(cmd)
+		for package in packages:
+
+			config = self.get_package(package)
+
+			for cmd in config['commands']:
+				if fnmatch.fnmatch(view.file_name(), cmd['write_path']):
+					self.run_prettier(view, cmd['options'], config['prettier_path'])
+					return
+
+		print(time.perf_counter() - self.start)
+		
+
+	def globPath(self, path, root):
+		return root + '/' + re.sub(r"^\./", "", path.replace('**/*', '*'))
+
+	def run_prettier(self, view, options, prettier_path):
+		cmd = [self.node_path, prettier_path] + options + ['--write', view.file_name()]
+		print(cmd)
 		subprocess.call(cmd)
-		#print(time.perf_counter() - self.start)
+		print(time.perf_counter() - self.start)
 
-	def get_package_json(self, view):
+	def get_packages(self, view):
+		packages = []
 		folder = os.path.dirname(view.file_name())
 		while folder != '/':
 			package = folder + '/package.json'
 			if os.path.exists(package):
-				return package
+				packages.append(package)
 			folder = os.path.dirname(folder)
-		return False
+		return packages
+
+	def get_package(self, path):
+		mtime = os.path.getmtime(path)
+
+		config = self.packages.get(path)
+
+		if not config or not config.get('mtime') == mtime:
+			config = self.parse_package(path)
+			config['mtime'] = mtime
+			self.packages[path] = config
+
+		return config
+
+	def parse_package(self, path):
+		print('parsing package ' + path)
+		jsonData = json.load(open(path, 'r'))
+		
+		scripts = jsonData['scripts']
+		cmds = []
+		root = os.path.dirname(path)
+
+		for key in scripts:
+			script = scripts[key]
+			script_parts = shlex.split(script)
+			
+			mode = 'LOOKING_FOR_PRETTIER'
+			
+			for i, v in enumerate(script_parts):
+				if mode == 'LOOKING_FOR_PRETTIER':
+					if v == 'prettier':
+						mode = 'PARSING_OPTIONS'
+						options = []
+				elif mode == 'PARSING_OPTIONS':
+					if (v == '&&' or v == ';' or v == '|' or v == '&' or v == '||' or v == '>' or v == '>>'):
+						write_path = options.pop()
+						cmds.append({ 'options': options, 'write_path': self.globPath(write_path, root) })
+						mode = 'LOOKING_FOR_PRETTIER'
+					elif v.endswith(';'):
+						v = re.sub(r";$", "", v)
+						cmds.append({ 'options': options, 'write_path': self.globPath(v, root) })
+						mode = 'LOOKING_FOR_PRETTIER'
+					elif len(script_parts) == i + 1:
+						cmds.append({ 'options': options, 'write_path': self.globPath(v, root) })
+					elif v != '--write':
+						options.append(v)
+
+		return {
+			'prettier_path': root + '/node_modules/.bin/prettier',
+			'root': root,
+			'commands': cmds
+		}
